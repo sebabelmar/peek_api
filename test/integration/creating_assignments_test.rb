316fca9a19160c9d2cccfb21475f3295721eec9b
sebabelmar <si.belmar@gmail.com>
2014-08-25 05:48:39 -0700

    Test to create controller done.

---
+++ test/integration/creating_assignments_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class CreatingAssignmentsTest < ActionDispatch::IntegrationTest
+  test 'creates boats' do
+    post '/api/assignments',
+    {boat:
+      {timeslot_id: 1, boat_id: 1}
+      }.to_json, {'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
+
+    assert_equal 201, response.status
+  end
+end

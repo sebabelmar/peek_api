1e8335c13dff0c52768fca672f3e6379450f965f
sebabelmar <si.belmar@gmail.com>
2014-08-25 03:40:02 -0700

    First Commit, after deleting las repo to add unit test

--- test/integration/creating_timeslots_test.rb
+++ test/integration/creating_timeslots_test.rb
@@ -4,7 +4,7 @@ class CreatingTimeslotsTest < ActionDispatch::IntegrationTest
   test 'creates timeslots' do
     post '/api/timeslots',
     {timeslot:
-      {start_time: 11111111, duration: 120}
+      {start_time: Time.now.to_i, duration: 120}
       }.to_json, {'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
 
     assert_equal 201, response.status
--- test/integration/listing_boats_test.rb
+++ test/integration/listing_boats_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class ListingTimeslotsTest < ActionDispatch::IntegrationTest
 
-  test 'returns list of all timeslots' do
-    get '/api/timeslots'
+  test 'returns list of all boats' do
+    get '/api/boats'
 
     assert_equal 200, response.status
     timeslots = JSON.parse(response.body, symbolize_names: true)

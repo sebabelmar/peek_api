6d19efd3df6edf53a1df64967666cf832374d661
sebabelmar <si.belmar@gmail.com>
2014-08-25 03:25:43 -0700

    Costum Routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ PeekApi::Application.routes.draw do
   get 'api/timeslots', :to => 'timeslots#index'
   post 'api/timeslots', :to => 'timeslots#create'
 
-  get 'api/boats', :to => 'boats#index'
-  post 'api/boats', :to => 'boats#create'
 
 
 end

fd33f8f73efcde3f1c1478b198c7ef3180d207df
sebabelmar <si.belmar@gmail.com>
2014-08-25 17:17:46 -0700

    Unix-daily-Range working in Controller. Ready to migrate to Model

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -1,10 +1,18 @@
+require 'date'
+
 class TimeslotsController < ApplicationController
   def index
     timeslots = Timeslot.all
-    timeslots.map! do |object|
-      hash = object.attributes.delete_if{|k, v| k == "created_at" || k == "updated_at"}
-      hash["boats"] = object.boats.to_a
-      object = hash
+
+
+    if begging_time = unix_day_min(params[:date])
+      timeslots = timeslots.where(start_time: begging_time..unix_day_max(params[:date]))
+
+      timeslots.map! do |object|
+        hash = object.attributes.delete_if{|k, v| k == "created_at" || k == "updated_at"}
+        hash["boats"] = object.boats.to_a
+        object = hash
+      end
     end
 
     render json: timeslots, status: 200
@@ -21,5 +29,23 @@ class TimeslotsController < ApplicationController
   private
   def timeslot_params
     params.require(:timeslot).permit(:start_time, :duration, :availability, :customer_count)
+  end
+
+  def date_to_unix(date)
+    date_arr = date.split('-').map!(&:to_i)
+    year, month, day = date_arr[0], date_arr[1], date_arr[2]
+    return Date.new(year, month, day).to_time.to_i
+  end
+
+  def unix_day_min(date)
+    date_to_unix(date)
+  end
+
+  def unix_day_max(date)
+    unix_day_min(date) + 86399
+  end
+
+  def unix_range?(date, start_time)
+    (date_to_unix(date)..unix_day_max(date)).include?(start_time)
   end
 end

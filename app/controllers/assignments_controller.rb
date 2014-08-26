357fb2e51216517c1af3969031e9b50885a6f6be
sebabelmar <si.belmar@gmail.com>
2014-08-25 18:59:40 -0700

    Availability test pssing. Callback when Assignment is save

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -3,6 +3,8 @@ class AssignmentsController < ApplicationController
     assignment = Assignment.new(assignment_params)
 
     if assignment.save
+      update_availability(assignment.timeslot_id)
+
       render json: assignment, status: 201
     end
   end
@@ -10,6 +12,13 @@ class AssignmentsController < ApplicationController
   private
   def assignment_params
     params.require(:assignment).permit(:timeslot_id, :boat_id)
+  end
+
+  def update_availability(timeslot_id)
+    timeslot = Timeslot.find(timeslot_id)
+    boats = timeslot.boats
+    total_availability = boats.map(&:capacity).reduce(:+)
+    timeslot.availability = total_availability
   end
 
 end
--- test/integration/creating_assignments_test.rb
+++ test/integration/creating_assignments_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class CreatingAssignmentsTest < ActionDispatch::IntegrationTest
   test 'creates assignments' do
+    timeslot = Timeslot.create(id: 1, start_time: 123456, duration: 120)
     post '/api/assignment',
     {assignment:
       {timeslot_id: 1, boat_id: 1}
